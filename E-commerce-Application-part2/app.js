dcb02930074ea1aff1b343a2060a2af2f2bee4d4
Arjun93 <nujra.an@gmail.com>
2015-12-14 23:39:25 -0500

    Db redirected to RDS

--- E-commerce-Application-part2/app.js
+++ E-commerce-Application-part2/app.js
@@ -37,9 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 var storeOptions = {
-    host: 'localhost',
-    //host: 'ecommerce.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
-    //port: 3306,
+    //host: 'localhost',
+    host: 'ecommerce.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
+    port: 3306,
     user: 'root',
     password: '12312312',
     database: 'ecommerce'
--- E-commerce-Application-part2/routes/dbConnection.js
+++ E-commerce-Application-part2/routes/dbConnection.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  //host     : 'ecommerce.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
-  //port : 3306,
+  //host     : 'localhost',
+  host     : 'ecommerce.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
+  port : 3306,
   user     : 'root',
   password : '12312312',
   database :'ecommerce',
